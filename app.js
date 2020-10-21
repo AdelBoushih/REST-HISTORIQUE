02a8a9c4f40d95afe3a214b32116c531968d9ab7
AdelBoushih <boushihadel96@gmail.com>
2020-10-21 23:21:21 +0100

    Updates

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const app = express();
+const bodyparser = require('body-parser');
+const mongoose = require('mongoose');
+require('dotenv/config');
+
+app.use(bodyparser.json());
 
 app.get('/' , (req,res) => {
     res.send('We\'re home !');
@@ -7,5 +12,10 @@ app.get('/' , (req,res) => {
 
 const historiqueRouter = require('./routes/historique');
 app.use('/historique' , historiqueRouter);
+
+mongoose.connect(process.env.DB_CONNECTION, 
+    { useNewUrlParser: true, useUnifiedTopology: true } , () => {
+    console.log('We\' re connected.');
+})
 
 app.listen('3000');
--- routes/historique.js
+++ routes/historique.js
@@ -1,9 +1,63 @@
 const express = require('express');
-
 const router = express.Router();
+const Historique = require('../models/historique');
 
-router.get('/' , (req,res) => {
-    res.send('We\'re on historique !');
+router.get('/' , async (req,res) => {
+    try{
+        const historique = await Historique.find();
+        res.json(historique);
+    }catch(err){
+        res.json({ message: err });
+    }
+});
+
+router.get('/:historiqueId' , async (req , res) => {
+    try{
+        const historique = await Historique.findById(req.params.historiqueId);
+        res.json(historique);
+    }catch(err){
+        res.json({ message: err })
+    }
+});
+
+router.post('/' , async (req,res) => {
+    const historique = new Historique({
+        num_programme: req.body.num_programme
+    });
+
+    try{
+        const savedhistorique = await historique.save();
+        res.json(savedhistorique);
+
+    }catch(err){
+        res.json({ message: err });
+    }
+    
+});
+
+router.delete('/:historiqueId' , async (req , res) => {
+    try{
+        /*const historique = await Historique.findById(req.params.historiqueId);
+        const deletedhistorique = historique.delete();*/
+        const deletedhistorique = await Historique.remove({ _id: req.params.historiqueId } );
+        res.json(deletedhistorique);
+    }catch(err){
+        res.json({ message:err });
+    }
+});
+
+router.patch('/:historiqueId' , async (req , res) => {
+    try{
+        /*const historique = await Historique.findById(req.params.historiqueId);
+        const editedhistorique = historique.patch({ $set:
+            {num_programme: req.body.num_programme}
+        })*/
+        const editedhistorique = await Historique.updateOne({
+            _id: req.params.historiqueId }, { $set: { num_programme:  req.body.num_programme } });
+        res.json(editedhistorique);
+    }catch(err){
+        res.json({ message: err });
+    }
 });
 
 module.exports = router;
